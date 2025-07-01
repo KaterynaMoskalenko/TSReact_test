9f9d10e2c6b5f6fa55f5a8ada62842000d3d7575
Kateryna Moskalenko <emochkaom@gmail.com>
2025-07-01 19:43:57 +0300

    tested import.meta.env.VITE_API_URL

--- src/store/ProjectSlice.js
+++ src/store/ProjectSlice.js
@@ -36,8 +36,8 @@ export const fetchProjects = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
         //const response = await axios.get(`http://localhost:3001/projects`);
-        const response = await axios.get("https://mock-api-legt.onrender.com/projects")
-       // const response = await axios.get(`${import.meta.env.VITE_API_URL}/projects`);
+        //const response = await axios.get("https://mock-api-legt.onrender.com/projects")
+       const response = await axios.get(`${import.meta.env.VITE_API_URL}/projects`);
 
         console.log(response);
         return response.data;
